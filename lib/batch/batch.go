55e1f0aa698a6363c5dfb5869c0e18bbf47a1785
Zeine <zeineshka2003@gmail.com>
2022-07-03 22:30:58 +0600

    conc test

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,18 +1,43 @@
 package batch
 
 import (
-	"time"
+	"fmt"
+	"sync"
 )
 
 type user struct {
 	ID int64
 }
 
-func getOne(id int64) user {
-	time.Sleep(time.Millisecond * 100)
-	return user{ID: id}
+func getOne(wg *sync.WaitGroup, poolNum int64, receive <-chan int64, response chan<- user) {
+	defer wg.Done()
+	for id := range receive {
+		fmt.Printf("worker-%d take element %d\n", poolNum, id)
+		response <- user{ID: id}
+	}
 }
 
-func getBatch(n int64, pool int64) (res []user) {
-	return nil
+func getBatch(n int64, pool int64) (res []user) { // n - num of users //pool - num of goroutines
+
+	wg := &sync.WaitGroup{}
+	wg.Add(int(pool))
+
+	//channel
+	receive := make(chan int64)
+	response := make(chan user)
+
+	for i := int64(0); i < pool; i++ {
+		go getOne(wg, i, receive, response)
+	}
+
+	for i := int64(0); i < n; i++ {
+		receive <- i
+		res = append(res, <-response)
+	}
+
+	//done
+	close(receive)
+	close(response)
+	wg.Wait()
+	return res
 }
