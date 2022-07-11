f476963f62ff9b5500afb4039725528b06d54337
ZeineI <zeineshka2003@gmail.com>
2022-07-11 13:50:17 +0600

    add errgroupZ

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -2,7 +2,6 @@ package batch
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
@@ -10,45 +9,73 @@ type user struct {
 	ID int64
 }
 
-func getOne(wg *sync.WaitGroup, poolNum int64, receive <-chan int64, response chan<- user) {
-	defer wg.Done()
+func getOne( /*wg *sync.WaitGroup,*/ poolNum int64, receive <-chan int64, response chan<- user) {
+	// defer wg.Done()
 	for id := range receive {
 		time.Sleep(time.Millisecond * 100)
-		fmt.Printf("Pool %d took user number %d", poolNum, id)
+		fmt.Printf("Pool %d took user number %d\n", poolNum, id)
 		response <- user{ID: id}
 	}
 }
 
 func getBatch(n int64, pool int64) (res []user) { // n - num of users //pool - num of goroutines
 
-	wg := &sync.WaitGroup{}
-	wg.Add(int(pool))
+	// wg := &sync.WaitGroup{}
+	// wg.Add(int(pool))
 
-	//channel
+	// channel
 	receive := make(chan int64, pool)
 	response := make(chan user, n)
 
 	for i := int64(0); i < pool; i++ {
-		go getOne(wg, i, receive, response)
+		go getOne( /*wg, */ i, receive, response)
 	}
 
 	for i := int64(0); i < n; i++ {
 		receive <- i
 	}
 
-	//done
+	// done
 
 	close(receive)
 
-	wg.Wait()
+	// wg.Wait()
 
 	for i := int64(0); i < n; i++ {
 		res = append(res, <-response)
 	}
 	close(response)
 
-	return res
+	return
 }
+
+// second implementation way
+
+// func getOne(id int64) user {
+// 	time.Sleep(time.Millisecond * 100)
+// 	return user{ID: id}
+// }
+
+// func getBatch(n int64, pool int64) (res []user) {
+// 	mx := &sync.Mutex{}
+
+// 	errG, _ := errgroup.WithContext(context.Background())
+// 	errG.SetLimit(int(pool))
+// 	for i := int64(0); i < 100; i++ {
+// 		iN := i
+// 		errG.Go(func() error {
+// 			resOne := getOne(iN)
+// 			mx.Lock()
+// 			defer mx.Unlock()
+// 			res = append(res, resOne)
+
+// 			return nil
+// 		})
+// 	}
+// 	_ = errG.Wait()
+
+// 	return res
+// }
 
 // func main() {
 // 	fmt.Println(getBatch(100, 10))
